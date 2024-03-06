93e60f147ac2f9b13a49bb2411603302dda6d083
WDWIZ <140268841+WDWIZ@users.noreply.github.com>
2024-03-06 17:17:42 +0900

    Update pages.jsx

--- src/pages/pages.jsx
+++ src/pages/pages.jsx
@@ -6,7 +6,7 @@ import NotFound from './404';
 
 const pages = [
     { "id" : "home", "path" : "/", "subtitle" : "동아리", "comp" : Home },
-    { "id" : "club", "path" : "/clubs", "subtitle" : "동아리 목록", "comp" : Club },
+    { "id" : "clubs", "path" : "/clubs", "subtitle" : "동아리 목록", "comp" : Club },
     { id: "admission", path: "/admission", subtitle: "동아리 신입생 모집", comp: Admission},
     { id: "users", path: "/users", subtitle: "Redirecting...", comp: Users},
     { "id" : "404", "path" : "*", "subtitle" : "404", "comp" : NotFound }
